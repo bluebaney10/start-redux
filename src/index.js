d861d8fa6c7da8844acd417edc263ee4f3388cdf
blue_baney <bluebaney10@gmail.com>
2019-01-01 16:49:32 +0700

    add multiple reducer and middle ware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import {createStore} from "redux";
+import {createStore,combineReducers,applyMiddleware} from "redux";
 
 const initialState={
     result:15000,
     value:[]
 }
 
-const reducer=(state=initialState,action)=>{
+const userReducer=(state={name:"Blue",age:31},action)=>{
+    switch(action.type){
+        case "setName":
+        state={
+            ...state,
+            name:action.payload
+        }
+        break;
+        
+        case "setAge":
+        state={
+            ...state,
+            age:action.payload   
+        }
+        break;
+        default:
+    }
+    return state;
+
+}
+
+
+const employeeReducer=(state=initialState,action)=>{
     switch(action.type){
         case "ADD":
         state={
-            ...state
+            ...state,
+            result:state.result+=action.payload,
+            value:[...state.value,action.payload]
             /*
+            ...state
+            or
             result:state.result,
             value:state.value
             */
@@ -20,21 +46,64 @@ const reducer=(state=initialState,action)=>{
         
         
         case "SUBTRACT":
-           // state-=action.payload;
+        state={
+            ...state,
+            result:state.result-=action.payload,
+            value:[...state.value,action.payload]
+            /*
+            ...state
+            or
+            result:state.result,
+            value:state.value
+            */
+        }
         break;
         default:
     }
     return state;
 }
 
-const store=createStore(reducer);
+//const store=createStore(EmployeeReducer);
+const mylogger=(store)=>(next)=>(action)=>{
+    console.log("Log Action ",action)
+    next(action)
+
+}
+const store=createStore(combineReducers({employeeReducer,userReducer}),{},applyMiddleware(mylogger));
+
 store.subscribe(()=>{
-    console.log("Update Store:",store.getState());
+   //console.log("Update Store:",store.getState());
 })
 store.dispatch({
     type:"ADD",
     payload:15000
 });
+store.dispatch({
+    type:"ADD",
+    payload:15000
+});
+store.dispatch({
+    type:"ADD",
+    payload:30000
+});
+
+store.dispatch({
+    type:"SUBTRACT",
+    payload:8000
+});
+
+store.dispatch({
+    type:"setName",
+    payload:"Redux"
+});
+
+store.dispatch({
+    type:"setAge",
+    payload:40
+});
+
+
+
 
 
 
